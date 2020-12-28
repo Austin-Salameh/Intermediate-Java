bb83b5bdfe622a09f7d8bb37d65d3c72a13536a0
SantoshiYakimoto <61394452+SantoshiYakimoto@users.noreply.github.com>
2020-12-28 14:59:24 -0600

    Add files via upload

---
+++ Week 1/example.java
@@ -0,0 +1,9 @@
+
+import javax.swing.JOptionPane;
+ public class example 
+ {
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null, "Example Dialog Box!");
+    }
+}
